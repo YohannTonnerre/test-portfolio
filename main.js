e27412da80baeac2c7749197192de6bbadbee0d8
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-04-29 11:59:57 +0200

    :sparkles: initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const loading = document.querySelector('.loading');
+const titleSpan1 = document.querySelector('.span-title1');
+const titleSpan2 = document.querySelector('.span-title2');
+const underline = document.querySelector('.underline');
+const btn = document.querySelector('.from-top');
+const chouette = document.querySelector('.chouette');
+const devWeb = document.querySelector('.dev-web');
+const container = document.querySelector('.first-view');
+const burger = document.querySelector('.burger-container');
+const logo = document.querySelector('.logo');
+
+
+
+
+
+
+function start(){
+	loading.style.animation = "loading 1s ease 1s forwards";
+	underline.style.animation = "underline 1.2s ease 1s";
+}
+
+loading.addEventListener("animationend", () => {
+	titleSpan1.style.animation = "down .5s ease .2s forwards";
+	titleSpan2.style.animation = "down .5s ease .4s forwards";
+	loading.style.display = "none";
+})
+
+
+titleSpan2.addEventListener("animationend", () => {
+	btn.style.animation = "pop 2s ease forwards";
+	titleSpan2.style.display = "none";
+	titleSpan1.style.display = "none";
+	container.style.flexDirection = "column";
+})
+
+
+
+btn.addEventListener("animationend", () => {
+	chouette.style.display = "inline";
+	devWeb.style.display = "inline";
+	chouette.style.animation = "goesDown .8s ease forwards";
+	devWeb.style.animation = "goesDown .8s ease forwards .2s";
+
+	logo.style.animation = "downed .8s ease forwards .2s";
+	burger.style.animation = "downed .8s ease forwards .2s";
+})
+
+window.onload = start();
