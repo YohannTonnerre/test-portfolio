9bde68ef29091f6deff1ea598932e18cabb323af
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-04-30 14:19:48 +0200

    :sparkles: ajout du menu fonctionnel et ajout de petites animations

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const loading = document.querySelector('.loading');
+
 const titleSpan1 = document.querySelector('.span-title1');
 const titleSpan2 = document.querySelector('.span-title2');
 const underline = document.querySelector('.underline');
+
 const btn = document.querySelector('.from-top');
+
 const chouette = document.querySelector('.chouette');
 const devWeb = document.querySelector('.dev-web');
+
 const container = document.querySelector('.first-view');
+
 const burger = document.querySelector('.burger-container');
+const burgerActive = document.querySelector('.burger-container.active');
+const menuContent = document.querySelector('.menu-content');
 const logo = document.querySelector('.logo');
 
 
+burger.addEventListener('click', ()=>{
+	menuContent.classList.toggle('active');
+	burger.classList.toggle('active');
+})
 
 
 
@@ -43,6 +54,20 @@ btn.addEventListener("animationend", () => {
 
 	logo.style.animation = "downed .8s ease forwards .2s";
 	burger.style.animation = "downed .8s ease forwards .2s";
+	menuContent.style.opacity = 1;
+})
+
+
+
+
+window.addEventListener("scroll", ()=>{
+	var scroll = window.pageYOffset;
+	console.log(scroll);
+	btn.style.opacity = 1 + -scroll / 250 ;
+	chouette.style.opacity = 1 + -scroll / 250 ;
+	devWeb.style.opacity = 1 + -scroll / 250 ;
+	chouette.style.marginLeft =  + scroll  + "px"; 
+	devWeb.style.marginRight =  + scroll  + "px"; 
 })
 
 window.onload = start();
