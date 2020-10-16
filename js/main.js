b589f34048c41bfbecc7800b676421cc343017e7
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-10-16 18:56:23 +0200

    :lipstick: improve style

--- js/main.js
+++ js/main.js
@@ -53,88 +53,86 @@ const passionImg3 = document.querySelector('.passion3-img');
 
 
 
-$('.burger-container, a').hover( ()=>{
-	cursor.classList.toggle('active');
+
+
+
+burger.addEventListener('click', () => {
+    menuContent.classList.toggle('active');
+    burger.classList.toggle('active');
+    html.classList.toggle('no-scroll');
 })
 
 
 
-burger.addEventListener('click', ()=>{
-	menuContent.classList.toggle('active');
-	burger.classList.toggle('active');
-	html.classList.toggle('no-scroll');
-})
 
 
 
-
-function start(){
-	loading.style.animation = "loading 1s ease 1s forwards";
-	underline.style.animation = "underlined 1.2s ease 1s";
-	html.classList.toggle('no-scroll');
+function start() {
+    loading.style.animation = "loading 1s ease 1s forwards";
+    underline.style.animation = "underlined 1.2s ease 1s";
+    html.classList.toggle('no-scroll');
 }
 
 loading.addEventListener("animationend", () => {
-	titleSpan1.style.animation = "down .5s ease .2s forwards";
-	titleSpan2.style.animation = "down .5s ease .4s forwards";
-	loading.style.display = "none";
-	/*$('.isloaded').removeClass('isloaded');*/
+    titleSpan1.style.animation = "down .5s ease .2s forwards";
+    titleSpan2.style.animation = "down .5s ease .4s forwards";
+    loading.style.display = "none";
+    /*$('.isloaded').removeClass('isloaded');*/
 })
 
 
 titleSpan2.addEventListener("animationend", () => {
-	btn.style.animation = "pop 2s ease forwards";
-	titleSpan2.style.display = "none";
-	titleSpan1.style.display = "none";
-	container.style.flexDirection = "column";
+    btn.style.animation = "pop 2s ease forwards";
+    titleSpan2.style.display = "none";
+    titleSpan1.style.display = "none";
+    container.style.flexDirection = "column";
 })
 
 
 
 btn.addEventListener("animationend", () => {
-	chouette.style.display = "inline";
-	devWeb.style.display = "inline";
-	chouette.style.animation = "goesDown .8s ease forwards";
-	devWeb.style.animation = "goesDown .8s ease forwards .2s";
+    chouette.style.display = "inline";
+    devWeb.style.display = "inline";
+    chouette.style.animation = "goesDown .8s ease forwards";
+    devWeb.style.animation = "goesDown .8s ease forwards .2s";
 
-	logo.style.animation = "downed .8s ease forwards .2s";
-	burger.style.animation = "downed .8s ease forwards .2s";
-	menuContent.style.opacity = 1;
-	html.classList.toggle('no-scroll');
+    logo.style.animation = "downed .8s ease forwards .2s";
+    burger.style.animation = "downed .8s ease forwards .2s";
+    menuContent.style.opacity = 1;
+    html.classList.toggle('no-scroll');
 })
 
 
 
 
-$(window).scroll(function () {
-	var scroll = window.pageYOffset;
-	var menuHeight = burger.pageYOffset;
-	btn.style.opacity = 1 + -scroll / 250 ;
-	chouette.style.opacity = 1 + -scroll / 250 ;
-	devWeb.style.opacity = 1 + -scroll / 250 ;
-	chouette.style.marginLeft =  + scroll / 2  + "px"; 
-	devWeb.style.marginRight =  + scroll / 2  + "px"; 
+$(window).scroll(function() {
+    var scroll = window.pageYOffset;
+    var menuHeight = burger.pageYOffset;
+    btn.style.opacity = 1 + -scroll / 250;
+    chouette.style.opacity = 1 + -scroll / 250;
+    devWeb.style.opacity = 1 + -scroll / 250;
+    chouette.style.marginLeft = +scroll / 2 + "px";
+    devWeb.style.marginRight = +scroll / 2 + "px";
 
 
-	if(scroll >= 100){
-		competence.classList.add('active');
-	}
+    if (scroll >= 100) {
+        competence.classList.add('active');
+    }
 
 
-	if ($('.competence ').isInViewport()) {
-	        $('.competence').addClass('active');
-	        $('.skill-box1').addClass('active');
-	        $('.skill-box2').addClass('active');
-	        $('.skill-box3').addClass('active');
-	    } 
-		else{
-			$('.competence').removeClass('active');
-			$('.skill-box1').removeClass('active');
-			$('.skill-box2').removeClass('active');
-			$('.skill-box3').removeClass('active');
-		}
+    if ($('.competence ').isInViewport()) {
+        $('.competence').addClass('active');
+        $('.skill-box1').addClass('active');
+        $('.skill-box2').addClass('active');
+        $('.skill-box3').addClass('active');
+    } else {
+        $('.competence').removeClass('active');
+        $('.skill-box1').removeClass('active');
+        $('.skill-box2').removeClass('active');
+        $('.skill-box3').removeClass('active');
+    }
 
-		/*if ($('.skill-box1 ').isInViewport()) {
+    /*if ($('.skill-box1 ').isInViewport()) {
 	        $('.skill-box1').addClass('active');
 	        $('.skill-box2').addClass('active');
 	        $('.skill-box3').addClass('active');
@@ -147,103 +145,77 @@ $(window).scroll(function () {
 		}*/
 
 
-		if ($('.creation-wrapper').isInViewport()) {
-			creationWrapper.style.transform = "translateY(" +  scroll/10 + "px)";
-		}
+    if ($('.creation-wrapper').isInViewport()) {
+        creationWrapper.style.transform = "translateY(" + scroll / 10 + "px)";
+    }
 
 
 
-		if ($('.black-title').isInViewport()) {
-	        $('.black-title').addClass('active');
-		}
-
-		else{
-			$('.black-title').removeClass('active');		
-		}
+    if ($('.black-title').isInViewport()) {
+        $('.black-title').addClass('active');
+    } else {
+        $('.black-title').removeClass('active');
+    }
 
 
 
 
-		if ($('.creation1').isInViewport()) {
-	        $('.creation1').addClass('active');
-	        $('.creation2').addClass('active');
-	        $('.creation3').addClass('active');
-	        $('.creation4').addClass('active');
-		}
-		else{
-			$('.creation1').removeClass('active');
-			$('.creation2').removeClass('active');
-			$('.creation3').removeClass('active');
-			$('.creation4').removeClass('active');
-		}
-
-
-		
-
-		if ($('.draw').isInViewport()){
-	        $('.draw').addClass('active');
-		}
-		else{
-	        $('.draw').removeClass('active');
-		}
-
-		if ($('.teset').isInViewport()){
-	        $('.teset').addClass('active');
-		}
-		else{
-	        $('.teset').removeClass('active');
-		}
-
-
-		if ($('.contact-title').isInViewport()){
-	        $('.contact-title').addClass('active');
-		}
-		else{
-	        $('.contact-title').removeClass('active');
-		}
-
-		
-		if ($('.contact-paragraph').isInViewport()){
-	        $('.contact-paragraph').addClass('active');
-		}
-		else{
-	        $('.contact-paragraph').removeClass('active');
-		}
-
-
-		if ($('.formation-title').isInViewport()){
-	        $('.formation-title').addClass('active');
-		}
-		else{
-	        $('.formation-title').removeClass('active');
-		}
+    if ($('.creation1').isInViewport()) {
+        $('.creation1').addClass('active');
+        $('.creation2').addClass('active');
+        $('.creation3').addClass('active');
+        $('.creation4').addClass('active');
+    } else {
+        $('.creation1').removeClass('active');
+        $('.creation2').removeClass('active');
+        $('.creation3').removeClass('active');
+        $('.creation4').removeClass('active');
+    }
 
 
 
-		
 
-	
+    if ($('.draw').isInViewport()) {
+        $('.draw').addClass('active');
+    } else {
+        $('.draw').removeClass('active');
+    }
 
+    if ($('.teset').isInViewport()) {
+        $('.teset').addClass('active');
+    } else {
+        $('.teset').removeClass('active');
+    }
+
+
+    if ($('.contact-title').isInViewport()) {
+        $('.contact-title').addClass('active');
+    } else {
+        $('.contact-title').removeClass('active');
+    }
+
+
+    if ($('.contact-paragraph').isInViewport()) {
+        $('.contact-paragraph').addClass('active');
+    } else {
+        $('.contact-paragraph').removeClass('active');
+    }
+
+
+    if ($('.formation-title').isInViewport()) {
+        $('.formation-title').addClass('active');
+    } else {
+        $('.formation-title').removeClass('active');
+    }
+})
+
+$('.contact-link').hover(()=>{
+	$('.contact-me').toggleClass('active');
 })
 
 
 
 
-/*passion1.addEventListener('mousemove', ()=>{
-	var x = event.clientX;
-	var y = event.clientY;
-	passionImg1.style.backgroundPosition =   x / 200 + "%" + y / 200 + "%";
-})
-
-passion2.addEventListener('mousemove', ()=>{
-	var x = event.clientX;
-	var y = event.clientY;
-	passionImg2.style.backgroundPosition =   x / 200 + "%" + y / 200 + "%";
-})
-
-passion1.scrollBloc = function(){
-	alert('aaa');
-}*/
 
 
 window.onload = start();
