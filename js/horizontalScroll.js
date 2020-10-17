44b854594383bc0fe1808b5ad36900250b42000b
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-10-17 18:41:07 +0200

    :lipstick: improve styles

--- js/horizontalScroll.js
+++ js/horizontalScroll.js
@@ -1,7 +1,10 @@
 const creation = document.querySelector('.creation-page-container');
 
 
+
+
 window.addEventListener('scroll', ()=> {
 	var scroll = window.pageYOffset;
 	creation.style.transform = "translateX(" + -scroll + "px)"; 
 })
+
